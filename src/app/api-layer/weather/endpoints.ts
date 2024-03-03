9c988cca5f4060020431c48ebd5e0b0c7c117994
Gonzalo Morales <gonzalomorales93@gmail.com>
2024-03-03 12:29:29 +0100

    use metrics units

--- src/app/api-layer/weather/endpoints.ts
+++ src/app/api-layer/weather/endpoints.ts
@@ -1,4 +1,5 @@
-const DOMAIN_URL = 'https://api.openweathermap.org'  // move to environment files
-const APP_KEY = '43c4c3a6b374fd7f70a1de7257bf9afe'
+const DOMAIN_URL = 'https://api.openweathermap.org'; // move to environment files
+const APP_KEY = '43c4c3a6b374fd7f70a1de7257bf9afe';
 
-export const WEATHER_URL = (lat: number, long: number): string =>  `${DOMAIN_URL}/data/2.5/weather?lat=${lat}&lon=${long}&appid=${APP_KEY}`
+export const WEATHER_URL = (lat: number, long: number): string =>
+  `${DOMAIN_URL}/data/2.5/weather?lat=${lat}&lon=${long}&appid=${APP_KEY}&units=metric`;
