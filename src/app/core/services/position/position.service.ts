8e09927d9e232054b91526f1491cae605915a31c
Gonzalo Morales <gonzalomorales93@gmail.com>
2024-03-03 12:10:10 +0100

    add position service

---
+++ src/app/core/services/position/position.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Observable, Subscriber } from 'rxjs';
+import { Position } from '../../../models/position.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PositionService {
+  public getPosition(): Observable<Position> {
+    return new Observable((observer: Subscriber<Position>) => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          observer.next({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          });
+          observer.complete();
+        });
+      } else {
+        observer.error();
+        observer.complete();
+      }
+    });
+  }
+}
