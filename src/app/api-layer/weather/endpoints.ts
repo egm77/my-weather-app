53d34b7827564bc2cab2ac8e85c596fce6c0bfdd
Gonzalo Morales <gonzalomorales93@gmail.com>
2024-03-03 13:57:49 +0100

    add forecast http service

--- src/app/api-layer/weather/endpoints.ts
+++ src/app/api-layer/weather/endpoints.ts
@@ -3,3 +3,6 @@ const APP_KEY = '43c4c3a6b374fd7f70a1de7257bf9afe';
 
 export const WEATHER_URL = (lat: number, long: number): string =>
   `${DOMAIN_URL}/data/2.5/weather?lat=${lat}&lon=${long}&appid=${APP_KEY}&units=metric`;
+
+export const FORECAST_URL = (lat: number, long: number): string =>
+  `${DOMAIN_URL}/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${APP_KEY}&units=metric`;
--- src/app/api-layer/weather/http/weather-http.service.ts
+++ src/app/api-layer/weather/http/weather-http.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { WeatherDto } from '../dtos/weather-dto';
-import { WEATHER_URL } from '../endpoints';
+import { FORECAST_URL, WEATHER_URL } from '../endpoints';
+import { ForecastDto } from '../dtos/forecast-dto';
 
 @Injectable({
   providedIn: 'root',
@@ -12,5 +13,12 @@ export class WeatherHttpService {
 
   public getCurrentWeather(lat: number, long: number): Observable<WeatherDto> {
     return this.httpClient.get<WeatherDto>(WEATHER_URL(lat, long));
+  }
+
+  public getForecastWeather(
+    lat: number,
+    long: number,
+  ): Observable<ForecastDto> {
+    return this.httpClient.get<ForecastDto>(FORECAST_URL(lat, long));
   }
 }
