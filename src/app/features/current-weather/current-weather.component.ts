276479f36bac537362ce481768e152b05fb8add0
Gonzalo Morales <gonzalomorales93@gmail.com>
2024-03-03 12:19:30 +0100

    get current weather

--- src/app/features/current-weather/current-weather.component.ts
+++ src/app/features/current-weather/current-weather.component.ts
@@ -1,8 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { finalize, switchMap } from 'rxjs';
+import { PositionService } from '../../core/services/position/position.service';
+import { WeatherApiService } from '../../api-layer/weather/api/weather-api.service';
+import { CurrentWeather } from '../../models/current-weather.interface';
 
 @Component({
   selector: 'app-current-weather',
   templateUrl: './current-weather.component.html',
   styleUrls: ['./current-weather.component.scss'],
 })
-export class CurrentWeatherComponent {}
+export class CurrentWeatherComponent implements OnInit {
+  public isLoading: boolean;
+  public isError: boolean;
+  public currentWeather: CurrentWeather;
+  constructor(
+    private positionService: PositionService,
+    private weatherService: WeatherApiService,
+  ) {}
+
+  ngOnInit() {
+    this.getCurrentWeather();
+  }
+
+  private getCurrentWeather(): void {
+    this.isLoading = true;
+    this.isError = false;
+    this.positionService
+      .getPosition()
+      .pipe(
+        finalize(() => (this.isLoading = false)),
+        switchMap((value) =>
+          this.weatherService.getCurrentWeather(
+            value.latitude,
+            value.longitude,
+          ),
+        ),
+      )
+      .subscribe(
+        (value) => {
+          this.currentWeather = value;
+          console.log(this.currentWeather);
+        },
+        () => {
+          this.isError = true;
+        },
+      );
+  }
+}
